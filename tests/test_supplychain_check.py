e77b346204058fe7bf9ebeec335235a53ad917a3
Karol Babioch <kbabioch@suse.de>
2018-05-30 17:05:25 +0200

    Increase code coverage for url checking module
    This adds tests for new methods and refines some existing tests to
    increase the code coverage in general.

--- tests/test_supplychain_check.py
+++ tests/test_supplychain_check.py
@@ -1,5 +1,6 @@
 import supplychain.check.url
 import supplychain.hardening.replacer
+import pytest
 
 availableUrlHttp = 'http://babioch.de'
 availableUrlHttps = 'https://babioch.de'
@@ -15,10 +16,28 @@ class TestSupplychainCheck:
   def test_isUnavailable(self):
     assert not supplychain.check.url.isAvailable(unavailableUrl)
 
-  def test_checker(self):
+  def test_urlChecker(self):
     checker = supplychain.check.url.URL(availableUrlHttp)
-    assert checker.isAvailableHttp()
-    assert checker.isAvailableHttps()
+    assert True == checker.isAvailableHttp()
+    assert True == checker.isAvailableHttps()
     assert checker.getHttp() == availableUrlHttp
     assert checker.getHttps() == availableUrlHttps
+
+  def test_isHttp(self):
+    checker = supplychain.check.url.URL(availableUrlHttp)
+    assert True == checker.isHttp()
+    assert False == checker.isHttps()
+
+  def test_isHttps(self):
+    checker = supplychain.check.url.URL(availableUrlHttps)
+    assert False == checker.isHttp()
+    assert True == checker.isHttps()
+
+  def test_invalidURL(self):
+    with pytest.raises(ValueError):
+      supplychain.check.url.URL('http://[')
+
+  def test_invalidURLScheme(self):
+    with pytest.raises(ValueError):
+      supplychain.check.url.URL('ftp://')
 
