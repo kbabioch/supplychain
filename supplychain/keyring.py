bfbc6fedb125fda577fd4bca92eb7b172714ff8a
Karol Babioch <kbabioch@suse.de>
2018-06-14 15:47:21 +0200

    Refinements to keyring

--- supplychain/keyring.py
+++ supplychain/keyring.py
@@ -17,50 +17,61 @@ import subprocess
 import sys
 import re
 
+# TODO Minimize functionality?
+
 class Keyring:
+
+    class GpgError(Exception):
+        pass
+
+    # https://git.gnupg.org/cgi-bin/gitweb.cgi?p=gnupg.git;a=blob_plain;f=doc/DETAILS
+    RE_PUB = re.compile(r'^pub:(?P<validity>.):(?P<key_length>\d+)+:(?P<key_algo>\d+):(?P<key_id>[A-Z0-9]+):')
+    RE_SIG = ''
 
     # TODO Construct homedir on the fly, context manager
     # TODO Create homedir if not specified
     # TODO Use --no-keyring?
     # Initialize homedir for GPG
     def __init__(self, homedir):
-        self.__homedir = homedir
-        self.__keyids = []
+        self.homedir = homedir
 
     # Run GPG
-    def __run_gpg(self, cmdline, stdout=subprocess.PIPE, stderr=subprocess.PIPE):
+    def gpg(self, cmdline, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE):
         # TODO Check whether can be found?
         # TODO Make this configurable?
         gpg = '/usr/bin/gpg'
-        cmdline = [gpg, '--homedir', self.__homedir] + cmdline
-        return subprocess.run(cmdline, stdout=stdout, stderr=stderr, check=True)
+        cmdline = [gpg, '--homedir', self.homedir, '--quiet', '--batch', '--yes', '--no-tty', '--no-greeting', '--with-colons', '--status-fd', '1', '--keyid-format', 'long'] + cmdline
+        return subprocess.Popen(cmdline, stdin=stdin, stdout=stdout, stderr=stderr, encoding=sys.getdefaultencoding())
 
-    # Extracts keyids from provided signature file and remembers them
-    def add_signature_file(self, signature_file):
-        output = self.__run_gpg(['--list-packets', signature_file]).stdout.decode(sys.stdout.encoding)
-        regexp = re.compile(r'^:signature packet: algo [0-9]+, keyid (?P<keyid>[0-9A-F]+)$', re.MULTILINE)
-        self.__keyids += re.findall(regexp, output)
-
-    # Receive keys for all collected keyids from keyserver
-    def receive_keys(self):
-        for keyid in self.__keyids:
-            self.receive_key(keyid)
+    # Extracts keyids from provided signature file and adds them to keyring
+    def add_key_from_signature_file(self, signature_file):
+        gpg = self.gpg(['--list-packets', signature_file])
+        for line in gpg.stdout:
+            print(line, end='')
+        # regexp = re.compile(r'^:signature packet: algo [0-9]+, keyid (?P<keyid>[0-9A-F]+)$', re.MULTILINE)
+        # for key in re.findall(regexp, output):
+            # self.receive_key(key)
 
     # Receive key for given keyid
     def receive_key(self, keyid):
-        self.__run_gpg(['--recv-key', keyid])
-
-    # Minimize all keys for keyids collected beforehand
-    def minimize(self):
-        for keyid in self.__keyids:
-            self.__run_gpg(['--no-tty', '--edit-key', keyid, 'minimize', 'save', 'quit'])
+        self.gpg(['--recv-key', keyid])
 
     # Export all keys to file (ASCII armor)
-    def export(self, filename):
+    def export_keyring(self, filename):
         with open(filename, 'w') as f:
-            self.__run_gpg(['--export', '--armor'], stdout=f)
+            self.gpg(['--export', '--armor'], stdout=f)
 
-    # String representation
-    def __str__(self):
-        return 'homedir: {}, keyids: {}'.format(self.__homedir, self.__keyids)
+    # Load keyring from file
+    def load_keyring(self, filename):
+        self.gpg(['--import', filename])
+
+    # List keys from keyring
+    def list_keys(self):
+        keys = []
+        gpg = self.gpg(['--list-keys'])
+        for line in gpg.stdout:
+            m = re.match(self.RE_PUB, line)
+            if m:
+                keys.append(m.group('key_id'))
+        return keys
 
