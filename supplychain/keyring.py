1610dcdab3ecebf638d08596eb6fc4f745953c2d
Karol Babioch <kbabioch@suse.de>
2018-07-12 16:36:07 +0200

    Implement keyring as context manager
    This implements the keyring as context manager. The homedir will be
    created temporarily whenever this class is initiated using the `with()`
    statement and will finally be destroyed, so this has not to be done from
    the outside.

--- supplychain/keyring.py
+++ supplychain/keyring.py
@@ -14,8 +14,10 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
+import shutil
 import subprocess
 import sys
+import tempfile
 import re
 
 logger = logging.getLogger(__name__)
@@ -29,14 +31,6 @@ class Keyring:
     # https://git.gnupg.org/cgi-bin/gitweb.cgi?p=gnupg.git;a=blob_plain;f=doc/DETAILS
     RE_PUB = re.compile(r'^pub:(?P<validity>.):(?P<key_length>\d+)+:(?P<key_algo>\d+):(?P<key_id>[A-Z0-9]+):')
     RE_SIG = re.compile(r'^:signature packet: algo [0-9]+, keyid (?P<key_id>[0-9A-F]+)$')
-
-    # TODO Construct homedir on the fly, context manager
-    # TODO Create homedir if not specified
-    # TODO Use --no-keyring?
-    # Initialize homedir for GPG
-    def __init__(self, homedir):
-        self.homedir = homedir
-        logger.info('Initializing keyring with GnuPG homedir: %s', homedir)
 
     # Run GPG
     def gpg(self, cmdline, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE):
@@ -96,3 +90,12 @@ class Keyring:
         logger.info('Listing keys from keyring: %s', keys)
         return keys
 
+    def __enter__(self):
+        homedir = tempfile.mkdtemp()
+        logger.info('Creating temporary GnuPG home directory: %s', homedir)
+        self.homedir = homedir
+        return self
+
+    def __exit__(self, type, value, traceback):
+        logger.info('Removing GnuPG homedir: %s', self.homedir)
+        shutil.rmtree(self.homedir)
